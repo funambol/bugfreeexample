38d2571872a6b928afe6d75e849b618ea3f6fd98
Stefano Fornari <stefano.fornari@gmail.com>
2016-01-26 13:48:52 +0100

    step6

--- src/test/java/bugfree/example/angular/BugFreeAngularController.java
+++ src/test/java/bugfree/example/angular/BugFreeAngularController.java
@@ -17,6 +17,7 @@
  */
 package bugfree.example.angular;
 
+import java.io.File;
 import java.io.IOException;
 import javax.script.ScriptException;
 import static org.assertj.core.api.BDDAssertions.then;
@@ -25,6 +26,7 @@ import org.mozilla.javascript.NativeArray;
 import org.mozilla.javascript.NativeObject;
 import ste.xtest.js.BugFreeEnvjs;
 import ste.xtest.js.JSAssertions;
+import ste.xtest.net.StubURLBuilder;
 
 /**
  *
@@ -34,15 +36,16 @@ public class BugFreeAngularController extends BugFreeEnvjs {
     public BugFreeAngularController() throws ScriptException, IOException {
         loadScript("/js/ecma5-adapter.js");
         loadScript("/js/angular-rhino.js");
+        loadScript("/js/envjs.urlstubber.js");
         loadScript("src/main/webapp/angular/js/controllers.js");
-        loadScript("src/test/js/angular/fake.js");
+        loadScript("src/test/angular/fake.js");
     }
     
     @Test
-    public void load_controller() throws Exception {
-        exec(
-            "controller('PhoneListCtrl', {$scope: scope});"
-        );
+    public void load_controller_and_data() throws Exception {
+        givenURLStubs("phones1.json");
+ 
+        exec("controller('PhoneListCtrl', {$scope: scope});");
         
         JSAssertions.then((NativeArray)exec("scope.phones;")).hasSize(3);
         NativeObject phone = (NativeObject)exec("scope.phones[0];");
@@ -51,6 +54,17 @@ public class BugFreeAngularController extends BugFreeEnvjs {
         phone = (NativeObject)exec("scope.phones[2];");
         then(phone.get("name", null)).isEqualTo("MOTOROLA XOOM™");
         then(phone.get("snippet", null)).isEqualTo("The Next, Next Generation tablet.");
+    }
+    
+    // --------------------------------------------------------- private methods
+    
+    private void givenURLStubs(final String file) throws Exception {
+        StubURLBuilder[] builders = prepareUrlStupBuilders(
+            "file://" + new File("phones/phones.json").getAbsolutePath()
+        );
+        System.out.println(">> " + "file://" + new File("phones/phones.json").getAbsolutePath());
+        
+        builders[0].file("src/test/angular/" + file).type("application/json");
     }
     
 }
